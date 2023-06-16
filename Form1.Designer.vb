88bf52fea239924b0c671d0e2d489487c2d0ab6a
Jeios8 <rjohngdc@gmail.com>
2023-06-16 07:02:06 -0700

    Added the countdown timer feature

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class form_Main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,28 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(form_Main))
+        timer_Countdown = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' timer_Countdown
+        ' 
+        timer_Countdown.Interval = 1000
+        ' 
+        ' form_Main
+        ' 
+        resources.ApplyResources(Me, "$this")
+        AutoScaleMode = AutoScaleMode.Font
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "form_Main"
+        ShowInTaskbar = False
+        TopMost = True
+        WindowState = FormWindowState.Maximized
+        ResumeLayout(False)
     End Sub
 
+    Friend WithEvents timer_Countdown As Timer
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,17 @@
-﻿Public Class Form1
+﻿
+Public Class form_Main
+    Private ticker As TimeSpan
 
+    Private Sub form_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ticker = TimeSpan.FromSeconds(15)
+    End Sub
+
+    Private Sub timer_Countdown_Tick(sender As Object, e As EventArgs) Handles timer_Countdown.Tick
+        ticker = ticker - TimeSpan.FromSeconds(1)
+
+        If ticker <= TimeSpan.Zero Then
+            MsgBox("Countdown timer is over! Initiating shutdown.")
+            Shell("shutdown /s /f /t 0")
+        End If
+    End Sub
 End Class
