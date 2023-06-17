5ba0efcd086908d43ea541fbc14802f6e7ffc96c
Jeios8 <rjohngdc@gmail.com>
2023-06-17 02:42:23 -0700

    Added COMListener and dummy button as signal detector.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,25 +25,71 @@ Partial Class form_Main
         components = New ComponentModel.Container()
         Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(form_Main))
         timer_Countdown = New Timer(components)
+        GroupBox1 = New GroupBox()
+        lbl_Timer = New Label()
+        btn_InsertCoin = New Button()
+        timer_COMListener = New Timer(components)
+        TextBox1 = New TextBox()
+        GroupBox1.SuspendLayout()
         SuspendLayout()
         ' 
         ' timer_Countdown
         ' 
         timer_Countdown.Interval = 1000
         ' 
+        ' GroupBox1
+        ' 
+        resources.ApplyResources(GroupBox1, "GroupBox1")
+        GroupBox1.BackColor = SystemColors.ActiveCaptionText
+        GroupBox1.Controls.Add(lbl_Timer)
+        GroupBox1.ForeColor = SystemColors.ActiveCaption
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.TabStop = False
+        ' 
+        ' lbl_Timer
+        ' 
+        resources.ApplyResources(lbl_Timer, "lbl_Timer")
+        lbl_Timer.Name = "lbl_Timer"
+        ' 
+        ' btn_InsertCoin
+        ' 
+        resources.ApplyResources(btn_InsertCoin, "btn_InsertCoin")
+        btn_InsertCoin.Name = "btn_InsertCoin"
+        btn_InsertCoin.UseVisualStyleBackColor = True
+        ' 
+        ' timer_COMListener
+        ' 
+        ' 
+        ' TextBox1
+        ' 
+        resources.ApplyResources(TextBox1, "TextBox1")
+        TextBox1.BackColor = SystemColors.WindowFrame
+        TextBox1.ForeColor = SystemColors.HighlightText
+        TextBox1.Name = "TextBox1"
+        ' 
         ' form_Main
         ' 
         resources.ApplyResources(Me, "$this")
         AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(TextBox1)
+        Controls.Add(btn_InsertCoin)
+        Controls.Add(GroupBox1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         MinimizeBox = False
         Name = "form_Main"
         ShowInTaskbar = False
-        TopMost = True
         WindowState = FormWindowState.Maximized
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents timer_Countdown As Timer
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lbl_Timer As Label
+    Friend WithEvents btn_InsertCoin As Button
+    Friend WithEvents timer_COMListener As Timer
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,17 +1,44 @@
 ﻿
-Public Class form_Main
-    Private ticker As TimeSpan
+Imports System.Net.Security
 
-    Private Sub form_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ticker = TimeSpan.FromSeconds(15)
+Public Class form_Main
+    Private TICKER As TimeSpan
+    Private WAIT_TIME = 120
+    Private NO_SIGNAL = True
+
+    Private Sub Form_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TICKER = TimeSpan.FromSeconds(WAIT_TIME)
     End Sub
 
-    Private Sub timer_Countdown_Tick(sender As Object, e As EventArgs) Handles timer_Countdown.Tick
-        ticker = ticker - TimeSpan.FromSeconds(1)
+    Private Sub Timer_Countdown_Tick(sender As Object, e As EventArgs) Handles timer_Countdown.Tick
+        TICKER -= TimeSpan.FromSeconds(1)
+        lbl_Timer.Text = TICKER.ToString("mm\:ss")
 
-        If ticker <= TimeSpan.Zero Then
+        If TICKER <= TimeSpan.Zero Then
+            timer_Countdown.Enabled = False
             MsgBox("Countdown timer is over! Initiating shutdown.")
-            Shell("shutdown /s /f /t 0")
+            'Shell("shutdown /s /f /t 0")
+        End If
+    End Sub
+
+    Private Sub btn_InsertCoin_Click(sender As Object, e As EventArgs) Handles btn_InsertCoin.Click
+        If NO_SIGNAL = True Then
+            NO_SIGNAL = False
+        Else
+            NO_SIGNAL = True
+        End If
+    End Sub
+
+    Private Sub timer_COMListener_Tick(sender As Object, e As EventArgs) Handles timer_COMListener.Tick
+        If NO_SIGNAL = True Then
+            If timer_Countdown.Enabled = False Then
+                TICKER = TimeSpan.FromSeconds(WAIT_TIME)
+                timer_Countdown.Enabled = True
+            End If
+        Else
+            If timer_Countdown.Enabled = True Then
+                timer_Countdown.Enabled = False
+            End If
         End If
     End Sub
 End Class
